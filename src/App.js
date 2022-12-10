70ad8d790a64473365213955eb9f4458c71a3c20
vicky-poonia <vickypoonia020@gmail.com>
2022-12-10 17:54:01 +0530

    progress check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./Components/About";
+import "./App.css";
+// import Blog from "./Components/Blog";
+// import Contact from "./Components/Contact";
+// import Footer from "./Components/Footer";
+// import Home from "./Components/Home";
+// import Project from "./Components/Project";
+// import Skills from "./Components/Skills";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Home /> */}
+      <About />
+      {/* <Skills /> */}
+      {/* <Project /> */}
+      {/* <Blog /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
 
 export default App;
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
