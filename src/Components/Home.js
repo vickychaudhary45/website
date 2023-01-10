8b2b926944fde77d018bb9b0d8126107532a7e32
vicky-poonia <vickypoonia020@gmail.com>
2023-01-10 12:09:01 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -107,7 +107,7 @@ function Home() {
                 I am currently based in Noida, Uttar Pradesh. <br />
               </h2>
               <div className="bttn">
-                <a href="https://drive.google.com/file/d/1kA-OkXWq5Mqr1A6XDlzZ5ivI2sOjm03N/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1dUD_8x6jSlqbsJYBxi5XihKv02iadU-T/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                   <button className="about btn pointer">My Resume</button>
                 </a>
               </div>
