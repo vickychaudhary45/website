45738de99bfe7bd3a38f67e56e9479f3f469fc5f
vicky-poonia <vickypoonia020@gmail.com>
2023-02-19 22:44:45 +0530

    Props
    Passing data as props in skills component with the use of functional component within functional component.

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -11,65 +11,30 @@ function Skills() {
 
       <div className="container">
         <div className="row">
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">Reactjs</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">React-Redux</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">React-Router</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">Javascript</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">HTML5</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">CSS3</h1>
-              </div>
-            </div>
-          </div>
-
-          <div className="col__3">
-            <div className="service__box pointer">
-              <div className="service__meta">
-                <h1 className="service__text">Figma</h1>
-              </div>
-            </div>
-          </div>
-
+          <Skill name={"Reactjs"} />
+          <Skill name={"React-Redux"} />
+          <Skill name={"React-Router-Dom"} />
+          <Skill name={"Javascript"} />
+          <Skill name={"HTML"} />
+          <Skill name={"CSS"} />
+          <Skill name={"Figma"} />
         </div>
       </div>
     </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <>
+      <div className="col__3">
+        <div className="service__box pointer">
+          <div className="service__meta">
+            <h1 className="service__text">{props.name}</h1>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
